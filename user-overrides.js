29eeec5987458c888c19868cc17a4d458bffa3aa
Oops <kenneth.r.rosen@gmail.com>
2022-09-03 09:05:38 +0000

    Update user-overrides.js

--- user-overrides.js
+++ user-overrides.js
@@ -1,8 +1,17 @@
-﻿/******
+/******
 *    name: journoQUBES arkenfox user-overrides.js
 *    date: 3 September 2022
-* version: 103
+*    version: 103
 *     url: https://github.com/kennethrrosen/
+******/
 
-//my overrides
+user_pref("browser.startup.page", 3); // 0102
+  // user_pref("browser.privatebrowsing.autostart", false); // 0110 required if you had it set as true
+  // user_pref("browser.sessionstore.privacy_level", 0); // 1003 optional to restore cookies/formdata
+user_pref("privacy.clearOnShutdown.history", false); // 2811
+  // user_pref("privacy.cpd.history", false); // 2820 optional to match when you use Ctrl-Shift-Del
+
+
+    
+  
 
