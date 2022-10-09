2f1090e9077ccc50c8938cecebd50bf1eaaf9e89
journoSEC <kenneth.r.rosen@gmail.com>
2022-10-09 03:06:00 +0000

    thunderbird flavor

---
+++ tb-user-overrides.js
@@ -0,0 +1,8 @@
+/******
+*    name: journoQUBES thunderbird user-overrides.js
+*    date: 3 September 2022
+*    version: 103
+*     url: https://github.com/kennethrrosen/journoQUBES/
+*    NOTES:
+*    These settings work for me and my threat level. They may break some websites. Refer back to arkenfox for greater assessments and sourcing on practicality of most.
+******/
