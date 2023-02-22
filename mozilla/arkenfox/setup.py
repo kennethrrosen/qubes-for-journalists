4df92696a5f275cea02e0562fac16594a26559b0
journoSEC <kenneth.r.rosen@gmail.com>
2023-02-22 12:17:22 +0100

    Update setup.py

--- mozilla/arkenfox/setup.py
+++ mozilla/arkenfox/setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='install_journoSEC_config',
+    name='install_journoSEC_firefox_config',
     version='1.0',
     description='Installs JournoSec Firefox configuration files',
     author='Your Name',
     author_email='you@example.com',
-    packages=['install_journoSEC_config'],
+    packages=['install_journoSEC_firefox_config'],
     install_requires=[],
     entry_points={
         'console_scripts': ['install_journoSEC_config=install_journoSEC_config.__main__:main']
