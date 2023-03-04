5adf56d1d71848d99551942a6fc8b100fdc52828
journoSEC <kenneth.r.rosen@gmail.com>
2023-03-04 07:59:04 +0100

    Create opnsense_report.py

---
+++ OPNsense/opnsense_report.py
@@ -0,0 +1,72 @@
+# prerequisites include postfix subprocess lm-sensors
+# and then add as cron job:
+#    Log in to the OPNsense web interface.
+#    Go to System > Settings > Cron.
+#    Click on "Add".
+#    Enter the following information:
+#        "User": root
+#        "Command": /usr/local/bin/python3 /path/to/opnsense_report.py
+#        "Description": OPNsense Report Script
+#        "Minute": 0
+#        "Hour": 6
+#        "Day of the Month": *
+#        "Month": *
+#        "Day of the Week": *
+#    Click on "Save".
+import subprocess
+import shutil
+
+# Define email addresses
+smtp_from = 'opnsense@example.com'
+smtp_to = 'admin@example.com'
+subject = input("Enter email subject: ")
+
+# Define shell command to collect all data
+shell_command = """
+echo "<h2>Local Disk Space/Usage:</h2>"
+df -h | sed -e 's/^/<pre>/' -e 's/$/<\\/pre>/' -e '1d'
+echo "<h2>CPU Power and Temperature:</h2>"
+sensors | sed -e 's/^/<pre>/' -e 's/$/<\\/pre>/'
+echo "<h2>AdBlockHome Statistics:</h2>"
+curl -s http://localhost:3000/api/status | jq '.' | sed -e 's/^/<pre>/' -e 's/$/<\\/pre>/'
+echo "<h2>IP Addresses:</h2>"
+ip -4 address show | sed -e 's/^/<pre>/' -e 's/$/<\\/pre>/'
+echo "<h2>Port Scanning Messages:</h2>"
+grep -i nmap /var/log/syslog | sed -e 's/^/<pre>/' -e 's/$/<\\/pre>/'
+echo "<h2>SSH Login Attempts:</h2>"
+grep sshd.*Failed /var/log/auth.log | sed -e 's/^/<pre>/' -e 's/$/<\\/pre>/'
+echo "<h2>Active LAN IPs:</h2>"
+arp -a | sed -e 's/^/<pre>/' -e 's/$/<\\/pre>/'
+echo "<h2>LAN Bandwidth Usage:</h2>"
+iftop -i lan0 -s 5 -t | sed -e 's/^/<pre>/' -e 's/$/<\\/pre>/'
+echo "<h2>WAN Bandwidth Usage:</h2>"
+iftop -i wan0 -s 5 -t | sed -e 's/^/<pre>/' -e 's/$/<\\/pre>/'
+echo "<h2>WiFi Bandwidth Usage:</h2>"
+iftop -i wlan0 -s 5 -t | sed -e 's/^/<pre>/' -e 's/$/<\\/pre>/'
+echo "<h2>WireGuard Tunnel Activity:</h2>"
+wg show all | sed -e 's/^/<pre>/' -e 's/$/<\\/pre>/'
+echo "<h2>Zenarmor Blocks:</h2>"
+grep block /var/log/syslog | sed -e 's/^/<pre>/' -e 's/$/<\\/pre>/'
+echo "<h2>DHCP Requests:</h2>"
+grep dhcp4 /var/log/syslog | sed -e 's/^/<pre>/' -e 's/$/<\\/pre>/'
+echo "<h2>Intrusion Detection Alerts:</h2>"
+suricatasc -c dump eve.json alert | sed -e 's/^/<pre>/' -e 's/$/<\\/pre>/'
+"""
+
+# Run shell command and collect output
+result = subprocess.run(['sh', '-c', shell_command], capture_output=True, text=True)
+
+# Construct the email message
+body = '<html><head></head><body><table border="1">'
+body += '<tr><th>Category</th><th>Details</th></tr>'
+
+for section, output in output_dict.items():
+    body += f'<tr><td>{section}</td><td><pre>{output}</pre></td></tr>'
+
+body += '</table></body></html>'
+message = f'To: {smtp_to}\nFrom: {smtp_from}\nSubject: {subject}\nContent-Type: text/html\n\n{body}'
+
+# Send the email using Postfix
+sendmail_command = ['/usr/sbin/sendmail', '-t']
+with subprocess.Popen(sendmail_command, stdin=subprocess.PIPE) as proc:
+    proc.communicate(message.encode())
